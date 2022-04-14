3ce623a0333c5e96dffe02447c38b0e89e6756ac
Megacodexz <megacodexz@gmail.com>
2022-04-14 10:39:43 +0700

    Change variable name

--- fine-tuning/loss.py
+++ fine-tuning/loss.py
@@ -1,6 +1,6 @@
 import torch
 
 
-def SILog(yhat, y, mask, lambda_term=0):
+def SILog(yhat, y, mask, lambda_=0):
     d = torch.log(yhat[mask] + 1e-8) - torch.log(y[mask] + 1e-8)
-    return (d ** 2).mean() - lambda_term * d.mean() ** 2
+    return (d**2).mean() - lambda_ * d.mean() ** 2
