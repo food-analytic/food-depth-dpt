c85874300736c16786ded90be73ba6471ea6da1b
Megacodexz <megacodexz@gmail.com>
2022-04-15 18:46:54 +0700

    Minor update loss

--- finetune/loss.py
+++ finetune/loss.py
@@ -2,5 +2,5 @@ import torch
 
 
 def SILog(yhat, y, mask, lambda_=0):
-    d = torch.log(yhat[mask] + 1e-8) - torch.log(y[mask] + 1e-8)
+    d = torch.log(yhat[mask] + 1e-8) - torch.log(y[mask])
     return (d**2).mean() - lambda_ * d.mean() ** 2
